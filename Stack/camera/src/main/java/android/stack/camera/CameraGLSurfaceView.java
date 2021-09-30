bd43bd70369538fb212e619065ec1a215bdb6a1f
chaozhouzhang <13760289294@139.com>
2021-09-30 20:49:35 +0800

    add stack
    add stack

---
+++ Stack/camera/src/main/java/android/stack/camera/CameraGLSurfaceView.java
@@ -0,0 +1,45 @@
+package android.stack.camera;
+
+import android.content.Context;
+import android.graphics.SurfaceTexture;
+import android.opengl.GLSurfaceView;
+import android.util.AttributeSet;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+public class CameraGLSurfaceView extends GLSurfaceView implements GLSurfaceView.Renderer, SurfaceTexture.OnFrameAvailableListener {
+
+    private Context mContext;
+    private SurfaceTexture mSurfaceTexture;
+    private int mTextureId = -1;
+
+
+    public CameraGLSurfaceView(Context context) {
+        super(context);
+    }
+
+    public CameraGLSurfaceView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    public void onFrameAvailable(SurfaceTexture surfaceTexture) {
+
+    }
+
+    @Override
+    public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+
+    }
+
+    @Override
+    public void onSurfaceChanged(GL10 gl, int width, int height) {
+
+    }
+
+    @Override
+    public void onDrawFrame(GL10 gl) {
+
+    }
+}
